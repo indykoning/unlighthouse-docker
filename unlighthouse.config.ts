a4bea5819eb1a4c9941719c19d51866a97bc1c71
indy koning <indy@justbetter.nl>
2023-05-20 15:05:35 +0200

    Add config options in case of proxying the frontend

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,8 +1,13 @@
 export default {
-  puppeteerOptions: {
-    args: ["--no-sandbox"],
-  },
-  server: {
-    open: false,
-  }
+    puppeteerOptions: {
+        args: ["--no-sandbox"],
+    },
+    server: {
+        open: false,
+    },
+    // In case you're proxying the frontend
+    // serverUrl: "http:/<site>/",
+    // clientUrl: "http://<site>/",
+    // apiUrl: "http://<site>/api",
+    // websocketUrl: "ws:///<site>/api/ws",
 }
